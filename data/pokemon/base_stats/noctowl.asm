149b15081b972fbca42da08a17d29a58f095a5b5
Lolo2374 <Loganjclinger@gmail.com>
2026-05-31 09:37:25 -0600

    Update noctowl.asm

--- data/pokemon/base_stats/noctowl.asm
+++ data/pokemon/base_stats/noctowl.asm
@@ -2,7 +2,7 @@ if DEF(FAITHFUL)
 	bst 452, 100,  50,  50,  86,  96,  70
 	;   bst   hp  atk  def  sat  sdf  spe
 else
-	bst 462, 100,  50,  50,  96,  96,  70
+	bst 472, 100,  50,  50,  96,  116,  70
 	;   bst   hp  atk  def  sat  sdf  spe
 endc
 
