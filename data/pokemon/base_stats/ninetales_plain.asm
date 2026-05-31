ece14380f0b165fe96444a03ce5071044f1cf82f
Lolo2374 <Loganjclinger@gmail.com>
2026-05-31 09:36:56 -0600

    Update ninetales_plain.asm

--- data/pokemon/base_stats/ninetales_plain.asm
+++ data/pokemon/base_stats/ninetales_plain.asm
@@ -2,7 +2,7 @@ if DEF(FAITHFUL)
 	bst 505,  73,  76,  75,  81, 100, 100
 	;   bst   hp  atk  def  sat  sdf  spe
 else
-	bst 505,  73,  67,  75,  91,  90, 109
+	bst 515,  73,  67,  75,  91,  100, 109
 	;   bst   hp  atk  def  sat  sdf  spe
 endc
 
