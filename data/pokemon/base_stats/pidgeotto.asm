06ee0681930713ab8cb505eabcd9ca27ab1c5875
Lolo2374 <Loganjclinger@gmail.com>
2026-05-31 09:41:20 -0600

    Update pidgeotto.asm

--- data/pokemon/base_stats/pidgeotto.asm
+++ data/pokemon/base_stats/pidgeotto.asm
@@ -6,7 +6,7 @@ else
 	;   bst   hp  atk  def  sat  sdf  spe
 endc
 
-	db NORMAL, FLYING ; type
+	db FLYING, FLYING ; type
 	db 120 ; catch rate
 	db 113 ; base exp
 	db NO_ITEM, NO_ITEM ; held items
