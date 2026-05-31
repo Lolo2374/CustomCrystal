3440eeb28ecdebcf2559f2423a10f3847e546f3d
Lolo2374 <Loganjclinger@gmail.com>
2026-05-31 09:25:41 -0600

    Update electabuzz.asm

--- data/pokemon/base_stats/electabuzz.asm
+++ data/pokemon/base_stats/electabuzz.asm
@@ -12,7 +12,7 @@ endc
 	db PERSIM_BERRY, ELECTIRIZER ; held items
 	dn GENDER_F25, HATCH_MEDIUM_SLOW ; gender ratio, step cycles to hatch
 
-	abilities_for ELECTABUZZ, STATIC, STATIC, VITAL_SPIRIT
+	abilities_for ELECTABUZZ, STATIC, IRON_FIST, VITAL_SPIRIT
 	db GROWTH_MEDIUM_FAST ; growth rate
 	dn EGG_HUMANSHAPE, EGG_HUMANSHAPE ; egg groups
 
